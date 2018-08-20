dbcca1fe13c3f8fe0e4dde56642c0ac4c0ea21d2
Zachary Spar <zachspar@Zacharys-MacBook-Pro-4.local>
2018-08-20 01:43:14 -0600

    Added timer code and readme

---
+++ timer/READEME.md
@@ -0,0 +1,8 @@
+# Simple Timer
+
+# Prequisites:
+ - pip
+ - python3
+
+## Run Program: 
+ - execute `python3 timer.py`
---
+++ timer/timer.py
@@ -0,0 +1,30 @@
+# """
+# Simple timer -- tkinter.
+
+# Zachary Spar <zachspar@gmail.com>
+# """
+import tkinter as tk
+
+
+counter = 0 
+def counter_label(label):
+  counter = 0
+  def count():
+    global counter
+    counter += 1
+    label.config(text=str(counter))
+    label.after(1000, count)
+  count()
+
+
+root = tk.Tk()
+root.title("Seconds")
+label = tk.Label(root, fg="red")
+label.pack()
+counter_label(label)
+button = tk.Button(root, text='Stop', fg='red', width=25, command=root.destroy)
+button.pack()
+root.mainloop()
+
+
+print("Total time (seconds):  " + str(counter))
